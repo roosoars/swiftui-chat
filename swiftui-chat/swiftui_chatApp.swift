d645dca3b57c614e820e8be1b3018c4dbc186806
Rodrigo Soares <rddoisgo@ufu.br>
2023-03-16 22:26:39 -0300

    Initial Commit

---
+++ swiftui-chat/swiftui_chatApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_chatApp.swift
+//  swiftui-chat
+//
+//  Created by Rodrigo Soares on 16/03/23.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_chatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
